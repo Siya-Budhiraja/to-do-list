871362b2bc1e493a3c18e08c3192d7dc06e95167
Siya Budhiraja <you@example.com>
2024-11-05 23:19:27 +0530

    My to do list

--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -7,7 +7,7 @@ const TodoItems = ({text, id, isComplete, deleteTodo, toggle}) => {
   return (
     <div className="flex items-centre my-3 gap-2">
         <div onClick={()=>{toggle(id)}} className="flex flex-1 items-centre cursor-pointer">
-            {/* {<img src={isComplete ? tick : not_tick} alt="" className="w-7" /> */} */}
+         {/*  {<img src={isComplete ? tick : not_tick} alt="" className="w-7" /> */  
             <p className="tex-slate-700 ml-4 text-[17px]">{text}</p>
         </div>
         {/* <img onClick={()=>{deleteTodo(id)}}src={delete_icon} alt="" className="w-3.5 cursor-pointer" /> */}
